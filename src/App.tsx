a27f53d64abfef99b62ca50ce2916bb8b0319014
Yael Sprikut <yael@yaelmbpro.local>
2023-01-27 01:05:51 -0500

    add dropdown option for updating country

--- src/App.tsx
+++ src/App.tsx
@@ -4,46 +4,53 @@ require('./scripts/dashboard')
 // import styled from 'styled-components';
 import Card from './components/Card'
 import { getTopHeadlines } from './scripts/client'
-var TinyURL = require('tinyurl');
+var TinyURL = require('tinyurl')
+import * as constants from './constants/dropdown-options'
 
 const shortenUrl = async (url: string) => {
-const response = await TinyURL.shorten(url);
-// console.log("RESPONSE: ", response)
-return response;
+    const response = await TinyURL.shorten(url)
+    // console.log("RESPONSE: ", response)
+    return response
 }
 
 interface Props {}
 
 interface State {
     articles: any
+    country: any
 }
-
-const COLOUR_ARRAY = [
-    'pink',
-    'purple',
-    'cyan',
-    'amber',
-    'blue',
-    'red',
-    'indigo',
-    'orange',
-    'green',
-]
 
 class App extends React.Component<Props, State> {
     constructor(props: any) {
         super(props)
+        this.change = this.change.bind(this)
         this.state = {
             articles: null,
+            country: null,
         }
     }
     async componentDidMount() {
-        const articleData = await getTopHeadlines()
-        articleData.articles.forEach(async(article: any) => {
-          if(article.urlToImage) {
-            const shortt = await shortenUrl(article.urlToImage)
-            article.backupImage = article.urlToImage.includes("cloudfront") ? shortt : shortt
-          }
+        const articleData = await getTopHeadlines('ca')
+        articleData.articles.forEach(async (article: any) => {
+            if (article.urlToImage) {
+                const shortt = await shortenUrl(article.urlToImage)
+                article.backupImage = article.urlToImage.includes('cloudfront')
+                    ? shortt
+                    : shortt
+            }
+        })
+        this.setState({ articles: articleData.articles })
+    }
+
+    async change(e: any) {
+        const articleData = await getTopHeadlines(e.target.value)
+        articleData.articles.forEach(async (article: any) => {
+            if (article.urlToImage) {
+                const shortt = await shortenUrl(article.urlToImage)
+                article.backupImage = article.urlToImage.includes('cloudfront')
+                    ? shortt
+                    : shortt
+            }
         })
         this.setState({ articles: articleData.articles })
     }
@@ -54,7 +61,11 @@ class App extends React.Component<Props, State> {
             this.state.articles.forEach((article: any, i: number) => {
                 showCard.push(
                     <Card
-                        color={COLOUR_ARRAY[i % COLOUR_ARRAY.length]}
+                        color={
+                            constants.COLOUR_ARRAY[
+                                i % constants.COLOUR_ARRAY.length
+                            ]
+                        }
                         headline={article.title}
                         img={article.urlToImage || article.backupImage}
                         url={article.url}
@@ -62,13 +73,46 @@ class App extends React.Component<Props, State> {
                     />
                 )
             })
+        function getCountryName(countryCode: string) {
+            if (constants.isoCountries.hasOwnProperty(countryCode)) {
+                // @ts-ignore
+                return constants.isoCountries[countryCode]
+            } else {
+                return countryCode
+            }
+        }
+        const dropdownCountry = () => {
+            const options = [] as any
+            constants.COUNTRIES.forEach(code => {
+                options.push(
+                    <option value={code}>{getCountryName(code)}</option>
+                )
+            })
+            return (
+                <select
+                    name="pets"
+                    id="pet-select"
+                    onChange={e => this.change(e)}
+                >
+                    <option value="">--Please choose an option--</option>
+                    {options}
+                </select>
+            )
+        }
         return (
-            <div
-                className="dashboard display-animation"
-                style={{ margin: '0 auto', width: '1130px' }}
-            >
-                {showCard}
-            </div>
+            <>
+                <div>
+                    <br />
+                    <label>Choose a country:&nbsp;</label>
+                    {dropdownCountry()}
+                </div>
+                <div
+                    className="dashboard display-animation"
+                    style={{ margin: '0 auto', width: '1130px' }}
+                >
+                    {showCard}
+                </div>
+            </>
         )
     }
 }
