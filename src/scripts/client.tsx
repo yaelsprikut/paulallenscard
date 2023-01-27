f8806f42b0214d410afa01db2e1c5513814f8e66
Yael Sprikut <yael@yaelmbpro.local>
2023-01-27 00:09:23 -0500

    get country by queryString

--- src/scripts/client.tsx
+++ src/scripts/client.tsx
@@ -1,7 +1,15 @@
 export const axios = require('axios').default
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const country = urlParams.get('country')
+console.log("country: ", country)
 
 export const getTopHeadlines = async () => {
-    const responseData = axios.get(process.env.REACT_APP_NEWS_API)
+    const responseData = axios.get(process.env.REACT_APP_NEWS_API, {
+      params: {
+        country: country ? country : 'ca'
+      }
+    })
     .then(function (response: any) {
       // console.log(JSON.stringify(response.data));
       return response.data;
