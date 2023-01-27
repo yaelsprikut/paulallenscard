27c35d2b9d80fe775d5372272b198dd3e4745013
Yael Sprikut <yael@yaelmbpro.local>
2023-01-26 22:21:02 -0500

    blah blah entertained somee folks

--- src/scripts/client.tsx
+++ src/scripts/client.tsx
@@ -3,7 +3,7 @@ export const axios = require('axios').default
 export const getTopHeadlines = async () => {
     const responseData = axios.get(process.env.REACT_APP_NEWS_API)
     .then(function (response: any) {
-      console.log(JSON.stringify(response.data));
+      // console.log(JSON.stringify(response.data));
       return response.data;
     })
     .catch(function (error: any) {
